7a1cb57bbb1d4cddc7321a3014b247191e576265
Maksym <mcs.max72@gmail.com>
2021-05-31 20:58:52 +0300

    first commit

---
+++ src/main/java/com/example/telegrambot/TelegramConstants.java
@@ -0,0 +1,6 @@
+package com.example.telegrambot;
+
+public class TelegramConstants {
+    public static final String BOT_NAME = "GoITJavaTest2021Bot";
+    public static final String BOT_TOKEN = "1836455316:AAF6Y-1cNViwA8uoG2DKvuF8-e7j2r3kj5o";
+}
